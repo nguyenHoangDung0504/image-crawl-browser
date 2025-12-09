ee9799855ec587e6c22355bbfe701fc05ad89846
nguyenHoangDung0504 <sdjklo2021@gmail.com>
2025-12-09 12:41:32 +0700

    Update: Save image with source

--- page_handlers/expose_handlers/default/saveSelectedImages.js
+++ page_handlers/expose_handlers/default/saveSelectedImages.js
@@ -1,3 +1,4 @@
+// @ts-check
 import fs from 'fs';
 import path from 'path';
 import { getPageImageRegistry } from '../../../resources/registries.js';
@@ -11,23 +12,33 @@ import { STORAGE_CONFIG } from '../../../user-configs.js';
 export default function saveSelectedImages(page, urls, folderPath) {
 	folderPath = folderPath ? folderPath : `${STORAGE_CONFIG.defaultStorage}/${Date.now()}/`;
 
+	const pageURL = page.url();
+
 	if (!urls || !urls.length || !folderPath) {
 		console.log('> [Error] Thiếu URL hoặc thư mục lưu.', folderPath);
 		return;
 	}
+
 	const imageReg = getPageImageRegistry(page);
 
 	fs.mkdirSync(folderPath, { recursive: true });
 
+	// Viết file .source.txt
+	const sourceFile = path.join(folderPath, '.source.txt');
+	fs.writeFileSync(sourceFile, pageURL + '\n', 'utf8');
+	console.log('> [SaveSelectedImages][Info] Đã ghi file nguồn:', sourceFile);
+
+	// Lưu ảnh
 	for (const url of urls) {
 		const buffer = imageReg.get(url);
 		if (!buffer) continue;
 
 		const fileName = path.basename(new URL(url).pathname) || 'image.jpg';
 		const fullPath = path.join(folderPath, fileName);
+
 		fs.writeFileSync(fullPath, buffer);
-		console.log('> [handler.SaveSelectedImages][Info] Đã lưu:', fullPath);
+		console.log('> [SaveSelectedImages][Info] Đã lưu:', fullPath);
 	}
 
-	console.log('> [handler.SaveSelectedImages][Info] Hoàn tất lưu ảnh vào:', folderPath);
+	console.log('> [SaveSelectedImages][Info] Hoàn tất lưu ảnh vào:', folderPath);
 }
